541ab5810cc83919ce63fddafc003dff7bc4366e
rupak-20 <rupakswami43887@gmail.com>
2022-06-20 15:47:17 +0530

    minor code improvements

--- Task-7/hair_seg/utils/image.py
+++ Task-7/hair_seg/utils/image.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import cv2 as cv
 
 def create_figure(img, mask, prediction, dye=False):
     return create_multi_figure([[img, mask, prediction]], dye)
@@ -29,21 +29,24 @@ def create_multi_figure(rows):
             mask = im
     
     img = img*255
+    hair_color_rgb = np.zeros((1,3))
     # img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
     # cv.imwrite('hair_mask.jpg', mask)
     # cv.imwrite('original_img.jpg', img)
-    img = img*mask
-    # cv.imwrite('hair_segment.jpg', img)
+
     total = 0
-    hair_color_rgb = np.zeros((1,3))
-    for i in range(len(img)):
-        for j in range(len(img[0])):
-            if img[i][j][0]!=0 and img[i][j][1]!=0 and img[i][j][2]!=0:
+    length = len(img)
+    width = len(img[0])
+    for i in range(length):
+        for j in range(width):
+            if mask[i][j][0]==0 and mask[i][j][1]==0 and mask[i][j][2]==0:
+                continue
+            else:
                 total += 1
                 hair_color_rgb = hair_color_rgb + img[i][j]
     
     hair_color_rgb = hair_color_rgb/total
     hair_color_rgb = hair_color_rgb.astype('int')
-    
+
     return hair_color_rgb
     
