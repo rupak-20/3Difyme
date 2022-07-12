b42a1e31d36ae184ac00b66d7e03ab65100bf762
Rupak <rupakswami43887@gmail.com>
2022-07-12 13:08:00 +0530

    updated for 3D

--- Task-9/task9.py
+++ Task-9/task9.py
@@ -2,25 +2,38 @@ import numpy as np
 
 
 # calculate barycentric coordinates
-def barycentric_coords(tri_a: np.mat, tri_b: np.mat, tri_c: np.mat, point: np.mat) -> np.mat:
-    AB = tri_b - tri_a
-    AC = tri_c - tri_a
-    BC = tri_c - tri_b
-    AP = point - tri_a
-    BP = point - tri_b
-    # area of a triangle is proportional to area of a parallellogram formed from 2 of congruent triangles
-    v = np.linalg.norm(np.cross(AB, AP))/np.linalg.norm(np.cross(AB, AC))
-    u = np.linalg.norm(np.cross(AC, AP))/np.linalg.norm(np.cross(AB, AC))
-    w = np.linalg.norm(np.cross(BC, BP))/np.linalg.norm(np.cross(AB, AC))
+def barycentric_coords(a: np.mat, b: np.mat, c: np.mat, d: np.mat, p: np.mat) -> np.mat:
+    pbcd = np.linalg.det(np.array([[p[0], b[0], c[0], d[0]],
+                                   [p[1], b[1], c[1], d[1]],
+                                   [p[2], b[2], c[2], d[2]],
+                                   [1, 1, 1, 1]]))
+    apcd = np.linalg.det(np.array([[a[0], p[0], c[0], d[0]],
+                                   [a[1], p[1], c[1], d[1]],
+                                   [a[2], p[2], c[2], d[2]],
+                                   [1, 1, 1, 1]]))
+    abpd = np.linalg.det(np.array([[a[0], b[0], p[0], d[0]],
+                                   [a[1], b[1], p[1], d[1]],
+                                   [a[2], b[2], p[2], d[2]],
+                                   [1, 1, 1, 1]]))
+    abcd = np.linalg.det(np.array([[a[0], b[0], c[0], d[0]],
+                                   [a[1], b[1], c[1], d[1]],
+                                   [a[2], b[2], c[2], d[2]],
+                                   [1, 1, 1, 1]]))
 
-    return [u, v, w]
+    u = pbcd/abcd
+    v = apcd/abcd
+    w = abpd/abcd
+    x = 1-u-v-w
+
+    return [u, v, w, x]
 
 
 # driver code
 if __name__ == '__main__':
-    a = np.array([10, 20, 39])
-    b = np.array([15, 12, 22])
-    c = np.array([9, 15, 29])
-    p = np.array([11, 13, 26])
+    a = np.array([0, 0, 0])
+    b = np.array([10, 0, 0])
+    c = np.array([0, 10, 0])
+    d = np.array([5, 5, 10])
+    p = np.array([15, 0, 0])
 
-    print(barycentric_coords(a, b, c, p))
+    print(barycentric_coords(a, b, c, d, p))
